01c2a549b46157ccb92125b1cd1411089643b630
Petrol95 <petrol195@yandex.ru>
2019-07-25 21:30:49 +0300

    deleted method change

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -11,16 +11,13 @@ public class BubbleSort {
     private static int[] bubbleSort(int[] arr) {
         for (int i = arr.length - 1; i > 0 ; i--) {
             for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1])
-                    change(arr, j, j + 1);
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
         return arr;
-    }
-
-    private static void change(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
