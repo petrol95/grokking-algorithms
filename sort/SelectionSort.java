df9d5e9480fe170d525d83b121348d4b75d6039f
Petrol95 <petrol195@yandex.ru>
2019-07-25 22:16:11 +0300

    refactoring: deleted method findSmallest, deleted second array

--- sort/SelectionSort.java
+++ sort/SelectionSort.java
@@ -1,37 +1,24 @@
 import java.util.Arrays;
 
 public class SelectionSort {
-    
-    private static int findSmallest(int[] arr) {
-        int smallest = arr[0];
-        int index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < smallest) {
-                smallest = arr[i];
-                index = i;
-            }
-        }
-        return index;
-    }
-    
-    private static void selectSort(int[] arr) {
-        int index;
-        int size = arr.length;
-        int[] newArr = new int[arr.length];
-        for (int i = 0; i < newArr.length; i++) {
-            index = findSmallest(arr);
-            newArr[i] = arr[index];
-            for (int j = index; j < size - 1; j++)
-                arr[j] = arr[j + 1];
-            size--;
-        }
-        System.out.println(Arrays.toString(newArr));
-    }
-
     public static void main(String[] args) {
         int[] arr = {5, 3, 6, 2, 10};
-        selectSort(arr);
+        System.out.println(Arrays.toString(selectSort(arr)));
         int[] arr1 = {-5, 7, 0, -6, 4, 0, 2, 5, 8, -3, 8};
-        selectSort(arr1);
+        System.out.println(Arrays.toString(selectSort(arr1)));
+    }
+
+    private static int[] selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int mark = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[mark])
+                    mark = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[mark];
+            arr[mark] = temp;
+        }
+        return arr;
     }
 }
