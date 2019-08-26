847946f2600b895b05d0a3a443789180b6e5d095
Petrol95 <petrol195@yandex.ru>
2019-08-26 21:42:48 +0300

    created class SynchBlock

--- threads/src/Counter.java
+++ threads/src/Counter.java
@@ -1,6 +1,9 @@
+/**
+ * Первый вариант синхронизации: монитор - экземпляр класса
+ */
+
 public class Counter {
     private int c;
-
     public Counter() {
         this.c = 0;
     }
@@ -8,12 +11,46 @@ public class Counter {
     public int value() {
         return c;
     }
-
     public synchronized void inc() {
         c++;
     }
-
     public synchronized void dec() {
         c--;
+    }
+
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.inc();
+//                    try {
+//                        Thread.sleep(1);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.dec();
+//                    try {
+//                        Thread.sleep(1);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(counter.value());
     }
 }
