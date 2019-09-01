854219e0ee7c05bde09516cf45cf57d4c8b3e5df
Petrol95 <petrol195@yandex.ru>
2019-09-01 21:35:18 +0300

    added classes ServerMain, Controller

---
+++ jchat/src/client/Controller.java
@@ -0,0 +1,70 @@
+package client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    final String SERVER_IP = "localhost";
+    final int SERVER_PORT = 8189;
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    @FXML
+    public TextArea textArea;
+    @FXML
+    public TextField msgField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            socket = new Socket(SERVER_IP, SERVER_PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            Thread t = new Thread(() -> {
+                try {
+                    while (true) {
+                        String msg = in.readUTF();
+                        textArea.appendText(msg + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            t.setDaemon(true);
+            t.start();
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void sendMsg() {
+        try {
+            out.writeUTF(msgField.getText());
+            msgField.clear();
+            msgField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
