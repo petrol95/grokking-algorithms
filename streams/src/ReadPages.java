bb27c013db3f1fd8580b9a5fff5a610c378cf4a3
Petrol95 <petrol195@yandex.ru>
2019-09-26 14:33:23 +0300

    added dateTime marks, file size check

--- streams/src/ReadPages.java
+++ streams/src/ReadPages.java
@@ -4,53 +4,71 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
+/**
+ * Написать консольное приложение которое умеет постранично читать текстовые файлы (размером > 10 Mb).
+ * Вводим страницу (за страницу можно принять 1800 символов), программа выводит ее в консоль.
+ * Контролируем время выполнения: программа не должна загружаться дольше 10 секунд, а чеение - занимать свыше 5 секунд.
+ */
 
 public class ReadPages {
 
-    public static final String FILE_NAME = "streams\\byteFile.txt";
-    public static final int PAGE_SIZE = 1800;
-
+    private static final String FILE_NAME = "streams\\bigFile.txt";
+    private static final int FILE_SIZE = 10 * 1024 * 1024; // bytes
+    private static final int PAGE_SIZE = 1800; // symbols
 
     public static void main(String[] args) throws IOException {
 
+        System.out.println("Preparing file : " + now());
+
         File file = new File(FILE_NAME);
         BufferedWriter wr = Files.newBufferedWriter(Paths.get(FILE_NAME));
+        int pageNum = 0;
         try {
-            for (int j = 1; j < PAGE_SIZE; j++) {
+            for (pageNum = 1; file.length() < FILE_SIZE; pageNum++) {
                 for (int i = 0; i < PAGE_SIZE; i++) {
-                    wr.write("" + j);
+                    wr.write("" + pageNum % 10);
                 }
             }
+            pageNum--;
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             wr.close();
         }
 
-        Scanner sc = new Scanner(System.in);
-        RandomAccessFile raf = new RandomAccessFile(FILE_NAME, "r");
-        try {
+        System.out.println("File prepared : " + now());
+
+        try (Scanner sc = new Scanner(System.in);
+             RandomAccessFile raf = new RandomAccessFile(FILE_NAME, "r")) {
             while (true) {
-                System.out.println("Введите номер страницы");
-                String msg;
-                int num;
-                msg = sc.nextLine();
+                System.out.println("Введите номер страницы (1..." + pageNum + "):");
+                String msg = sc.nextLine();
                 if (msg.equals("end")) break;
                 else {
-                    num = Integer.parseInt(msg);
-                    raf.seek((num - 1) * PAGE_SIZE);
-                    for (int i = 0; i < PAGE_SIZE; i++) {
-                        System.out.println((char) raf.read());
+                    int num = Integer.parseInt(msg);
+                    if (num < 0 || num > pageNum) {
+                        System.out.println("Введен неверный номер страницы!");
+                        break;
                     }
+                    raf.seek((num - 1) * PAGE_SIZE);
+                    System.out.println("Reading page : " + now());
+                    for (int i = 0; i < PAGE_SIZE; i++) {
+                        System.out.print((char) raf.read());
+                    }
+                    System.out.println();
+                    System.out.println("Page reading finished : " + now());
                 }
             }
         } catch (Exception e) {
             System.out.println("Ошибка ввода!");
-        } finally {
-            sc.close();
-            raf.close();
         }
+    }
 
+    private static String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("hh:MM:ss"));
     }
 }
