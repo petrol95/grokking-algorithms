8c084762e8f53a7db745c99af9e4cf030b05c374
Petrol95 <petrol195@yandex.ru>
2019-09-29 17:31:32 +0300

    used ExecutionService in class WriteFile

--- threads/src/WriteFile.java
+++ threads/src/WriteFile.java
@@ -1,6 +1,10 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Написать небольшой метод, в котором 3 потока построчно пишут данные в файл (по 10 записей с периодом в 20 мс)
@@ -8,54 +12,60 @@ import java.io.IOException;
 
 public class WriteFile {
 
-    static BufferedWriter wr;
+    public static final int COUNT = 10;
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        wr = new BufferedWriter(new FileWriter("threads/output.txt", true));
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(3);
 
-        Thread t1 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    write("AAAAAAAAAA");
-                    Thread.sleep(20);
-                } catch (InterruptedException | IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+                "threads/output.txt", false)))) {
+            service.scheduleAtFixedRate(() -> write(out, "AAAAAAAAAA"), 0, 20, TimeUnit.MILLISECONDS);
+            service.scheduleAtFixedRate(() -> write(out, "BBBBBBBBBB"), 0, 20, TimeUnit.MILLISECONDS);
+            service.scheduleAtFixedRate(() -> write(out, "CCCCCCCCCC"), 0, 20, TimeUnit.MILLISECONDS);
+            Thread.sleep(1000);
+        }
+        service.shutdown();
 
-        Thread t2 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    write("BBBBBBBBBB");
-                    Thread.sleep(20);
-                } catch (InterruptedException | IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        Thread t3 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    write("CCCCCCCCCC");
-                    Thread.sleep(20);
-                } catch (InterruptedException | IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        t1.start();
-        t2.start();
-        t3.start();
-        Thread.sleep(3000);
-        wr.close();
+//        // вариант 2
+//        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+//                "threads/output.txt", false)))) {
+//            Thread t1 = new Thread(() -> {
+//            try {
+//                write(out, "AAAAAAAAAA");
+//                Thread.sleep(20);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        Thread t2 = new Thread(() -> {
+//            try {
+//                write(out, "BBBBBBBBBB");
+//                Thread.sleep(20);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        Thread t3 = new Thread(() -> {
+//            try {
+//                write(out, "CCCCCCCCCC");
+//                Thread.sleep(20);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t1.join();
+//        t2.join();
+//        t3.join();
+//        }
     }
 
-    private static synchronized void write(String s) throws IOException {
-        wr.write(s);
-        wr.newLine();
+    private static void write(PrintWriter out, String s) {
+        for (int i = 0; i < COUNT; i++) {
+            out.println(s);
+        }
     }
 }
