25e9e5f9218b70c12372b1001433ed045356740d
Petrol95 <petrol195@yandex.ru>
2019-11-09 22:44:24 +0300

    added examples of using Optional type

--- datastreams/src/OptionalTest.java
+++ datastreams/src/OptionalTest.java
@@ -4,6 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 public class OptionalTest {
@@ -17,6 +18,23 @@ public class OptionalTest {
                 .filter(s -> s.contains("fred"))
                 .findFirst();
         System.out.println(optionalValue.orElse("No word") + " contains fred");
+
+        Optional<String> optionalString = Optional.empty();
+        String result = optionalString.orElse("N/A");
+        System.out.println("result: " + result);
+        result = optionalString.orElseGet(() -> Locale.getDefault().getDisplayName());
+        System.out.println("result: " + result);
+        try {
+            result = optionalString.orElseThrow(IllegalStateException::new);
+            System.out.println("result: " + result);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        optionalValue = wordList.stream()
+                .filter(s -> s.contains("red"))
+                .findFirst();
+        optionalValue.ifPresent(s -> System.out.println(s + " contains red"));
 
 
     }
