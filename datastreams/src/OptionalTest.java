0f42038a2be26351cda4a1264df4a5d1d4ea2845
Petrol95 <petrol195@yandex.ru>
2019-11-10 21:51:44 +0300

    added examples of using Optional type

--- datastreams/src/OptionalTest.java
+++ datastreams/src/OptionalTest.java
@@ -2,10 +2,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.Optional;
+import java.util.*;
 
 public class OptionalTest {
 
@@ -36,6 +33,26 @@ public class OptionalTest {
                 .findFirst();
         optionalValue.ifPresent(s -> System.out.println(s + " contains red"));
 
+        Set<String> results = new HashSet<>();
+        optionalValue.ifPresent(results::add);
+        Optional<Boolean> added = optionalValue.map(results::add);
+        System.out.println(added);
 
+        System.out.println(inverse(4.0).flatMap(OptionalTest::squareRoot));
+        System.out.println(inverse(-1.0).flatMap(OptionalTest::squareRoot));
+        System.out.println(inverse(0.0).flatMap(OptionalTest::squareRoot));
+
+        Optional<Double> result2 = Optional.of(-4.0)
+                .flatMap(OptionalTest::inverse)
+                .flatMap(OptionalTest::squareRoot);
+        System.out.println(result2);
+    }
+
+    private static Optional<Double> inverse(Double x) {
+        return x == 0 ? Optional.empty() : Optional.of(1 / x);
+    }
+
+    private static Optional<Double> squareRoot(Double x) {
+        return x < 0 ? Optional.empty() : Optional.of(Math.sqrt(x));
     }
 }
