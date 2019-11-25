6c1cc8c10aaa34b01f78a84ed45eddc766c54f6e
Petrol95 <petrol195@yandex.ru>
2019-11-25 22:50:36 +0300

    added examples of closures

--- lambda/src/Closure.java
+++ lambda/src/Closure.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 public class Closure {
     public static void main(String[] args) {
         repeatMessage("Hello", 1000);
+        countDown(10, 1000);
+        repeat("Bye", 3);
         JOptionPane.showMessageDialog(null, "Quit program?");
         System.exit(0);
     }
@@ -15,5 +17,23 @@ public class Closure {
             Toolkit.getDefaultToolkit().beep();
         };
         new Timer(delay, listener).start();
+    }
+
+    public static void countDown(int start, int delay) {
+        ActionListener listener = event -> {
+//            start--; // error! variable should be final
+            System.out.println(start);
+        };
+        new Timer(delay, listener).start();
+    }
+
+    public static void repeat(String text, int count) {
+        for (int i = 1; i <= count; i++) {
+            ActionListener listener = event -> {
+//                System.out.println(i + ": " + text); // error! variable should be final
+                System.out.println(text);
+            };
+            new Timer(1000, listener).start();
+        }
     }
 }
