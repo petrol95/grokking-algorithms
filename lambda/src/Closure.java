9aa67c3f76a9247fff709c1a323456ebe121931e
Petrol95 <petrol195@yandex.ru>
2019-11-26 23:39:38 +0300

    added an error example of closure

--- lambda/src/Closure.java
+++ lambda/src/Closure.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 
 public class Closure {
     public static void main(String[] args) {
-        repeatMessage("Hello", 1000);
-        countDown(10, 1000);
-        repeat("Bye", 3);
-        JOptionPane.showMessageDialog(null, "Quit program?");
-        System.exit(0);
+//        repeatMessage("Hello", 1000);
+//        countDown(10, 1000);
+//        repeat("Bye", 3);
+//        JOptionPane.showMessageDialog(null, "Quit program?");
+//        System.exit(0);
+
+//        Path first = Paths.get("/usr/bin");
+//        Comparator<String> comp = (first, second) -> first.length() - second.length(); // error: first is already defined
     }
 
     private static void repeatMessage(String text, int delay) {
