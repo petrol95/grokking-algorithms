76f531f7abeb3b92a50f73199630cdea4cf77fb7
Petrol95 <petrol195@yandex.ru>
2020-01-03 23:06:57 +0300

    added classes Employee and SAXExample

---
+++ xml/src/SAXexample/SAXExample.java
@@ -0,0 +1,38 @@
+package SAXexample;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SAXExample {
+    private static ArrayList<Employee> employees = new ArrayList<>();
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+
+        XMLHandler handler = new XMLHandler();
+        parser.parse(new File("xml/employees/employee.xml"), handler);
+
+        for (Employee employee : employees)
+            System.out.println(String.format("Имя сотрудника: %s, его должность: %s", employee.getName(), employee.getJob()));
+    }
+
+    private static class XMLHandler extends DefaultHandler {
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (qName.equals("employee")) {
+                String name = attributes.getValue("name");
+                String job = attributes.getValue("job");
+                employees.add(new Employee(name, job));
+            }
+        }
+    }
+}
