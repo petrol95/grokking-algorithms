a4b7e1107a3d69960f08bd0d717622bc056b1c4f
Petrol95 <petrol195@yandex.ru>
2020-01-04 23:18:10 +0300

    added class SAXExample_2

---
+++ xml/src/SAXexample/SAXExample_2.java
@@ -0,0 +1,59 @@
+package SAXexample;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SAXExample_2 {
+    private static ArrayList<Employee> employees = new ArrayList<>();
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+
+        AdvancedXMLHandler handler = new AdvancedXMLHandler();
+        parser.parse(new File("xml/employees/employee_2.xml"), handler);
+
+        for (Employee employee : employees)
+            System.out.println(String.format("Имя сотрудника: %s, его должность: %s", employee.getName(), employee.getJob()));
+    }
+
+    private static class AdvancedXMLHandler extends DefaultHandler {
+        private String name, job, lastElementName;
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            lastElementName = qName;
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            String information = new String(ch, start, length);
+
+            information = information.replace("\n", "").trim();
+
+            if (!information.isEmpty()) {
+                if (lastElementName.equals("name"))
+                    name = information;
+                if (lastElementName.equals("job"))
+                    job = information;
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if ( (name != null && !name.isEmpty()) && (job != null && !job.isEmpty()) ) {
+                employees.add(new Employee(name, job));
+                name = null;
+                job = null;
+            }
+        }
+    }
+}
