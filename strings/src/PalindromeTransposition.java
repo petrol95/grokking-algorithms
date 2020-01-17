e58b68008a56deb3e6a030fe1ee22330dbafd32e
Petrol95 <petrol195@yandex.ru>
2020-01-17 23:59:29 +0300

    correcting algorithm

--- strings/src/PalindromeTransposition.java
+++ strings/src/PalindromeTransposition.java
@@ -5,19 +5,45 @@
 public class PalindromeTransposition {
     public static void main(String[] args) {
         String s = "Tact Coa";
-        System.out.println(s + " : " + checkTransposition(s));
+        System.out.println(s + " : " + checkTransposition(s, 2));
     }
 
-    private static boolean checkTransposition(String s) {
-        char[] arr = s.toCharArray();
-        for (int i = 2; i < arr.length; i++) {
-            if (PalindromeCheck.isPalindrome(transport(arr, i)))
-                return true;
+    private static boolean checkTransposition(String s, int pos) {
+        if (pos == s.length())
+            return false;
+        if (PalindromeCheck.isPalindrome(s))
+            return true;
+        else {
+            String string1 = s.substring(0, pos);
+            String string2 = s.substring(pos, s.length());
+            char[] arr = string1.toCharArray();
+
+            for (int i = 0; i < arr.length - 1; i++) {
+                arr = transport(arr, i);
+                String newString = arr.toString() + string2;
+                if (PalindromeCheck.isPalindrome(newString))
+                    return true;
+                checkTransposition(newString, pos + 1);
+            }
         }
         return false;
     }
 
-    private static String transport(char[] arr, int i) {
-        return arr.toString();
+    private static char[] transport(char[] arr, int pos) {
+        switch (pos) {
+            case 0:
+                return swap(arr, arr.length - 2, arr.length - 1);
+            case 1:
+                return swap(arr, arr.length - 1, 0);
+            default:
+                return swap(arr, pos - 2, pos - 1);
+        }
+    }
+
+    private static char[] swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return arr;
     }
 }
