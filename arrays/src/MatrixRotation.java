448e0890567f50099df7c7fe534dc81e50492e66
Petrol95 <petrol195@yandex.ru>
2020-01-31 00:07:41 +0300

    fixed bugs

--- arrays/src/MatrixRotation.java
+++ arrays/src/MatrixRotation.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class MatrixRotation {
     public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
         for (int i = 0; i < arr[0].length; i++) {
             System.out.println(Arrays.toString(arr[i]));
         }
@@ -15,16 +15,22 @@ public class MatrixRotation {
     }
 
     private static void rotateFrame(int[][] arr, int size) {
-        if (size == 1) {
+        if (size < 2) {
             return;
         }
-        for (int i = 0; i < size - 1; i++) {
-            int temp = arr[0][i];
+        int temp;
+        temp = arr[0][0];
+        arr[0][0] = arr[size - 1][0];
+        arr[size - 1][0] = arr[size - 1][size - 1];
+        arr[size - 1][size - 1] = arr[0][size - 1];
+        arr[0][size - 1] = temp;
+        for (int i = 1; i < size - 1; i++) {
+            temp = arr[0][i];
             arr[0][i] = arr[i][0];
             arr[i][0] = arr[size - 1][size - i - 1];
             arr[size - 1][size - i - 1] = arr[size - i - 1][size - 1];
             arr[size - i - 1][size - 1] = temp;
         }
-        rotateFrame(arr, size - 1);
+        rotateFrame(arr, size - 2);
     }
 }
