aa4779427b11935196e08f59365c080ba6905329
Petrol95 <petrol195@yandex.ru>
2020-02-04 23:36:13 +0300

    fixed bugs

--- arrays/src/MatrixRotation.java
+++ arrays/src/MatrixRotation.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 public class MatrixRotation {
     public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] arr = {{10, 11, 12, 13, 14}, {15, 16, 17, 18, 19}, {20, 21, 22, 23, 24},
+                {25, 26, 27, 28, 29}, {30, 31, 32, 33, 34}};
         for (int i = 0; i < arr[0].length; i++) {
             System.out.println(Arrays.toString(arr[i]));
         }
@@ -19,23 +20,23 @@ public class MatrixRotation {
             return;
         }
         int temp;
-        temp = arr[0 + offset][0 + offset];
-        arr[0 + offset][0 + offset] = arr[size - 1 - offset][0 + offset];
-        arr[size - 1 - offset][0 + offset] = arr[size - 1 - offset][size - 1 - offset];
-        arr[size - 1 - offset][size - 1 - offset] = arr[0 + offset][size - 1 - offset];
-        arr[0 + offset][size - 1 - offset] = temp;
+        temp = arr[offset][offset];
+        arr[offset][offset] = arr[size - 1 + offset][offset];
+        arr[size - 1 + offset][offset] = arr[size - 1 + offset][size - 1 + offset];
+        arr[size - 1 + offset][size - 1 + offset] = arr[offset][size - 1 + offset];
+        arr[offset][size - 1 + offset] = temp;
 
         for (int i = 1 + offset; i < size - 1; i++) {
-            temp = arr[0 + offset][i + offset];
-            arr[0 + offset][i + offset] = arr[size - i - 1 - offset][0 + offset];
-            arr[size - i - 1 - offset][0 + offset] = arr[size - 1 - offset][size - i - 1 - offset];
+            temp = arr[offset][i + offset];
+            arr[offset][i + offset] = arr[size - i - 1 - offset][offset];
+            arr[size - i - 1 - offset][offset] = arr[size - 1 - offset][size - i - 1 - offset];
             arr[size - 1 - offset][size - i - 1 - offset] = arr[i + offset][size - 1 - offset];
             arr[i + offset][size - 1 - offset] = temp;
         }
-        System.out.println();
-        for (int i = 0; i < arr[0].length; i++) {
-            System.out.println(Arrays.toString(arr[i]));
-        }
+//        System.out.println();
+//        for (int i = 0; i < arr[0].length; i++) {
+//            System.out.println(Arrays.toString(arr[i]));
+//        }
         rotateFrame(arr, size - 2, 1);
     }
 }
