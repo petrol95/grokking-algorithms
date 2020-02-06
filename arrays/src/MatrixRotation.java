15b3e629f8acd2ec1ddd1ab4fab945068b3cf588
Petrol95 <petrol195@yandex.ru>
2020-02-06 22:32:47 +0300

    fixed bugs

--- arrays/src/MatrixRotation.java
+++ arrays/src/MatrixRotation.java
@@ -1,5 +1,9 @@
 import java.util.Arrays;
 
+/**
+ * Matrix 90 degree rotation
+ */
+
 public class MatrixRotation {
     public static void main(String[] args) {
         int[][] arr = {{10, 11, 12, 13, 14}, {15, 16, 17, 18, 19}, {20, 21, 22, 23, 24},
@@ -7,12 +11,15 @@ public class MatrixRotation {
         for (int i = 0; i < arr[0].length; i++) {
             System.out.println(Arrays.toString(arr[i]));
         }
-//        System.out.println(Arrays.deepToString(arr));
-        rotateFrame(arr, arr[0].length, 0);
+        rotateFrame(arr);
         System.out.println();
         for (int i = 0; i < arr[0].length; i++) {
             System.out.println(Arrays.toString(arr[i]));
         }
+    }
+
+    private static void rotateFrame(int[][] arr) {
+        rotateFrame(arr, arr[0].length, 0);
     }
 
     private static void rotateFrame(int[][] arr, int size, int offset) {
@@ -20,26 +27,26 @@ public class MatrixRotation {
             return;
         }
         int[] temp = new int[size];
+        // first horizontal
         for (int i = 0; i < size; i++) {
             temp[i] = arr[offset][i + offset];
         }
-
+        // first vertical
         for (int i = size - 1, j = 0; j < size; i--, j++) {
-            arr[offset][i] = arr[j][offset];
+            arr[offset][i + offset] = arr[j + offset][offset];
         }
-
+        // last horizontal
         for (int i = 0; i < size; i++) {
-            arr[i][0] = arr[size - 1][i];
+            arr[i + offset][offset] = arr[size - 1 + offset][i + offset];
         }
-
+        // last vertical
         for (int i = 0, j = size - 1; i < size; i++, j--) {
-            arr[size - 1][i] = arr[j][size - 1];
+            arr[size - 1 + offset][i + offset] = arr[j + offset][size - 1 + offset];
         }
-
+        // first horizontal
         for (int i = 0, j = size - 1; i < size; i++, j--) {
-            arr[i][size - 1] = temp[i];
+            arr[i + offset][size - 1 + offset] = temp[i];
         }
-
         rotateFrame(arr, size - 2, 1);
     }
 }
