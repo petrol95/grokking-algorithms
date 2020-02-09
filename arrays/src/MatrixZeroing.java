f1bb0205ba4e86df1dfda17f40d26baf7bb5f7ef
Petrol95 <petrol195@yandex.ru>
2020-02-09 23:51:46 +0300

    fixed bugs

--- arrays/src/MatrixZeroing.java
+++ arrays/src/MatrixZeroing.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 public class MatrixZeroing {
     public static void main(String[] args) {
-        int[][] arr = {{10, 11, 12, 13, 14}, {15, 16, 0, 18, 19}, {20, 21, 22, 23, 24}};
+        int[][] arr = {{10, 11, 12, 13, 14}, {15, 16, 17, 18, 19}, {20, 21, 22, 23, 24}, {26, 27, 28, 29, 0},
+                {31, 32, 33, 34, 35}};
         for (int i = 0; i < arr.length; i++) {
             System.out.println(Arrays.toString(arr[i]));
         }
@@ -14,9 +15,10 @@ public class MatrixZeroing {
     }
 
     private static void checkZero(int[][] arr) {
+        int[][] copyArr = Arrays.copyOf(arr, arr.length);
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
-                if (arr[i][j] == 0) {
+                if (copyArr[i][j] == 0) {
                     makeZero(arr, i, j);
                 }
             }
@@ -24,7 +26,7 @@ public class MatrixZeroing {
     }
 
     private static void makeZero(int[][] arr, int row, int col) {
-        for (int i = 0; i < arr[0].length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (i == row) {
                 for (int j = 0; j < arr[0].length; j++) {
                     arr[i][j] = 0;
